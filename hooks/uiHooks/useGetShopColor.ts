d0d6099282332154001cbb7394c91b35451e0892
Murilo L Mattioli <44042977+murilomattioli@users.noreply.github.com>
2021-10-11 06:40:36 -0300

    update uiHooks

--- hooks/uiHooks/useGetShopColor.ts
+++ hooks/uiHooks/useGetShopColor.ts
@@ -1,22 +1,21 @@
 import { useRouter } from "next/dist/client/router";
 import { useCallback } from "react";
 
-export type ShopColorNames = 'Water';
-export const SHOP_COLORS: Array<ShopColorNames> = ['Water'];
+export type ShopTypes = 'Water';
+export const SHOP_COLORS: Array<ShopTypes> = ['Water'];
 
-type ReturnProps = [() => ShopColorNames | undefined]
+type ReturnProps = [() => ShopTypes | undefined]
 
 export default (): ReturnProps => {
   const router = useRouter();
   
-  const handleShopColor = useCallback((): ShopColorNames | undefined => {
-    const shopColor = <ShopColorNames>`${router.basePath}`.replace('/', '').replace('Shop', '');
+  const handleShopColor = useCallback((): ShopTypes | undefined => {
+    const shopColor = <ShopTypes>`${router.basePath}`.replace('/', '').replace('Shop', '');
     const isValidShopColor = SHOP_COLORS.includes(shopColor);
     if (isValidShopColor) {
       return shopColor;
     };
   }, [router]);
-
 
   return [handleShopColor];
 };
